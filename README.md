72f72aed00bc251130e3aa14e9dbe9dab282c36e
Bryan Bonvallet <btbonval@gmail.com>
2017-07-30 18:07:57 -0400

    initial import

---
+++ README.md
@@ -0,0 +1,24 @@
+Remove From Anything Outfit
+ny Randolph Cherrypepper
+
+This mod is meant for XML modders.
+
+There is an outfit in RimWorld called Anything. It is the default outfit assigned to all pawns. At game start, all apparel in the defs is added to the Anything outfit.
+
+RFAO will prevent the Anything outfit from initially allowing apparel with RemoveFromAnything in the defaultOutfitTags list. This is mostly useful for mods with apparel that might be dangerous to wear.
+
+RFAO only affects the initial Anything outfit generated at game start. The player can still modify the Anything outfit to allow apparel with RemoveFromAnything in the defaultOutfitTags list.
+
+Requires HugsLib for Harmony.
+
+Compatibility note: RFAO applies a Postfix patch to RimWorld.OutfitDatabase.GenerateStartingOutfits(). In other words, it should be safe to mix with other mods without issue.
+
+Mod Order: Anywhere below HugsLib.
+
+Feel free to leave a comment if you run into a problem.
+
+Steam Download:
+http://steamcommunity.com/sharedfiles/filedetails/?id=1095539883
+
+GitHub Download:
+https://github.com/RandolphCherrypepper/RemoveFromAnythingOutfit/releases/
---
+++ Source/Cherrypepper_RemoveFromAnythingOutfit/Cherrypepper_RemoveFromAnythingOutfit/Patches.cs
@@ -0,0 +1,67 @@
+﻿using RimWorld;
+using Verse;
+using Harmony;
+using System.Reflection;
+
+namespace Randolph_Cherrypepper
+{
+    [StaticConstructorOnStartup]
+    class Main
+    {
+        static Main()
+        {
+            var harmony = HarmonyInstance.Create("com.randolphcherrypepper.rimworld.mod.removefromanythingoutfit");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(OutfitDatabase), "GenerateStartingOutfits")]
+    public static class CherrypepperPatch_RimWorld_OutfitDatabase_GenerateStartingOutfits
+    {
+        [HarmonyPostfix]
+        public static void RemoveFromAnythingOutfit(OutfitDatabase __instance)
+        {
+            if (__instance == null)
+            {
+                Log.Warning("RemoveFromAnythingOutfit cannot access OutfitDatabase.");
+                return;
+            }
+
+            string outfitAnythingLabel = "OutfitAnything".Translate();
+            if (outfitAnythingLabel == null || outfitAnythingLabel.Length == 0)
+            {
+                Log.Warning("RemoveFromAnythingOutfit cannot determine a valid label for OutfitAnything.");
+                return;
+            }
+
+            // Find the Anything outfit. It should be the first, but let's be proper about it and check the identifying label.
+            Outfit outfitAnything = null;
+            foreach (Outfit outfit in __instance.AllOutfits)
+            {
+                if (outfit.label == outfitAnythingLabel)
+                {
+                    outfitAnything = outfit;
+                    break;
+                }
+            }
+            if (outfitAnything == null)
+            {
+                Log.Warning("RemoveFromAnythingOutfit cannot find the Anything outfit in the OutfitDatabase.");
+                return;
+            }
+
+            // We should have the Anything outfit now. Let sort all apparel with RemoveFromAnything and disallow it.
+            int count = 0;
+            foreach (ThingDef thingdef in DefDatabase<ThingDef>.AllDefs)
+            {
+                if (thingdef.apparel != null && thingdef.apparel.defaultOutfitTags != null && thingdef.apparel.defaultOutfitTags.Contains("RemoveFromAnything"))
+                {
+                    outfitAnything.filter.SetAllow(thingdef, false);
+                    count += 1;
+                }
+            }
+            if (count == 0)
+                Log.Message("RemoveFromAnythingOutfit found no Apparel with RemoveFromAnything defaultOutfitTag.");
+        }
+    }
+}
